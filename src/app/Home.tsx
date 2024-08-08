26a5640e25b051dc337b62e20cd1008c76b11239
prerakkaushik <prerak.kaushik@gmail.com>
2024-08-08 14:05:37 -0400

    Added routing support

---
+++ src/app/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      {/* <header className="navbar-header">
+        <h2>Header</h2>
+      </header> */}
+      <div>
+        <h1>Home</h1>
+      </div>
+      {/* <footer className="navbar-footer">
+        <Button>Footer Button</Button>
+      </footer> */}
+    </div>
+  );
+}
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import ReactDOM  from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
 import App from './App.tsx'
 import './index.css'
+import Home from './app/Home.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <FluentProvider theme={webLightTheme}>
-        <App />
+      <Router>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/home" element={<Home />}>
+                {/* <Navbar /> */}
+                {/* <Route path="subroute1" element={<subComp1 />} />
+                <Route path="subroute2" element={<subComp2 />} /> */}
+              </Route>
+            </Routes>
+      </Router>
       </FluentProvider>
   </React.StrictMode>,
 )
