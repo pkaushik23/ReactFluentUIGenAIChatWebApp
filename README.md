e8db51fad6a6efb4cc85935bddb8d548528bebfe
prerakkaushik <prerak.kaushik@gmail.com>
2024-08-08 14:21:37 -0400

    useNavigateHook : linked home page from app main page

--- README.md
+++ README.md
@@ -60,4 +60,13 @@ export default {
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
 
+-----------
+## Trail
+
+1. Integrate FluentUI
+2. Enable Routing
+3. useNavigateHook
+
+
+
 
--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import { Button } from '@fluentui/react-components';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useNavigate } from 'react-router-dom';
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const navigate = useNavigate();
+
+  const gotoHome = () => {
+    navigate('/home');
+  };
 
   return (
     <>
@@ -20,20 +26,20 @@ function App() {
         </a>
       </div>
       <h2>Built with Vite + React</h2>
+      
       <div className="card center">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <Button appearance="primary">Get started</Button>
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p> */}
-        
+        <Button appearance="primary" onClick={() => gotoHome() }>Get started</Button>
       </div>
+
       <p className="read-the-docs center">
           learn more
       </p>
-      
+
+      {/* <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
+      </button> */}
+
+
     </>
   )
 }
