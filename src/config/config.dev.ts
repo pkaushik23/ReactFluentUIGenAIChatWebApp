51e47f92111719acfbba0d6fa74aefd759089465
prerakkaushik <prerak.kaushik@gmail.com>
2024-09-06 22:55:46 -0400

    using .env for storing config values.

--- src/config/config.dev.ts
+++ src/config/config.dev.ts
@@ -14,9 +14,9 @@ export const graphConfig = {
 
 export const msalConfig = {
     auth: {
-        clientId: "",
-        authority: "https://login.microsoftonline.com/<>",
-        redirectUri: "http://localhost:5173/",
+        clientId: import.meta.env.VITE_MSAL_CLIENT_ID,
+        authority: `https://login.microsoftonline.com/${import.meta.env.VITE_MSAL_TENANT_ID}`,
+        redirectUri: import.meta.env.VITE_MSAL_REDIRECT_URI,
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
